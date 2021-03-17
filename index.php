972bfb0331a384b27a4deb50ba98a3c90d7e4058
Ivana-995 <ilalic110@gmail.com>
2021-03-17 15:12:16 +0100

    single entry point

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
+
+session_start();
+
+define('BP', __DIR__ . DIRECTORY_SEPARATOR
+             . 'app' . DIRECTORY_SEPARATOR);
+

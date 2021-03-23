30f06caee103f87070f1c2dd9e880324e29b491a
Ivana-995 <ilalic110@gmail.com>
2021-03-23 22:59:16 +0100

    postavljanje autorizacije

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -14,6 +14,50 @@ class IndexController extends Controller
 
     public function login()
     {
-        $this->view->render('login');
+        $this->loginView('','');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['autoriziran']);
+        session_destroy();
+        $this->index();
+    }
+
+    public function autorizacija()
+    {
+        if(!isset($_POST['email']) || !isset($_POST['lozinka'])){
+            $this->login();
+            return; //short curcuiting
+        }
+
+        if(strlen(trim($_POST['email']))===0){
+            $this->loginView('','Obavezno email');
+            return;
+        }
+
+        if(strlen(trim($_POST['lozinka']))===0){
+            $this->loginView($_POST['email'],'Obavezno lozinka');
+            return;
+        }
+
+        if(!($_POST['email']==='edunova@edunova.hr' && 
+        $_POST['lozinka']==='e') ){
+            $this->loginView($_POST['email'],'Neispravna kombinacija emaila i lozinke');
+            return;
+    }
+
+    $_SESSION['autoriziran']='Edunova Korisnik';
+    $np = new NadzornaplocaController();
+    $np->index();
+
+    }
+
+    private function loginView($email,$poruka)
+    {
+        $this->view->render('login',[
+            'email'=>$email,
+            'poruka'=>$poruka
+        ]);
     }
 }

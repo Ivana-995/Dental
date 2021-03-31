c0f8ee23653ddcf9449d5720344fc6fdcba722c1
Ivana-995 <ilalic110@gmail.com>
2021-04-01 00:02:33 +0200

    izmjena usluge u odnosu na promjenu konstrukcije

--- app/controller/UslugaController.php
+++ app/controller/UslugaController.php
@@ -7,75 +7,61 @@ class UslugaController extends AutorizacijaController
                         . 'usluga' 
                         . DIRECTORY_SEPARATOR;
 
+    private $entitet = null;
+    private $poruka = '';
+
     public function index()
     {
         $this->view->render($this->viewDir . 'index',
         [
-            'usluge'=> Usluga::ucitajSve()
+            'entiteti'=> Usluga::ucitajSve()
         ]);
     }
 
     public function novo()
     {
         if($_SERVER['REQUEST_METHOD']==='GET')
-        {
-            $usluga = new stdClass();
-            $usluga->naziv='';
-            $usluga->vrsta='ddd';
-            $usluga->cijena=1000;
-            $this->novoView($usluga,'Popunite sva polja');
-            return;
-        }
-
-        $usluga = (object) $_POST;   
-        
-        if(strlen(trim($usluga->naziv))===0)
-        { 
-             $this->novoView($usluga,'Naziv obavezno');
-             return;
-   
-
-        }
-
-        if(strlen(trim($usluga->naziv))>50)
-        { 
-             $this->novoView($usluga,'Naziv ne može imati više od 50 znakova');
-            return;
-        }
-
-        if(strlen(trim($usluga->vrsta))===0)
-        { 
-             $this->novoView($usluga,'Vrsta obavezno');
-             return;
-   
-
-        }
-
-        if(strlen(trim($usluga->vrsta))>50)
-        { 
-             $this->novoView($usluga,'Vrsta ne može imati više od 50 znakova');
-             return;
-        }
-
-        $usluga->cijena=str_replace(',','.',$usluga->cijena);
-        if(!is_numeric($usluga->cijena)
-        || ((float)$usluga->cijena)<=0)
-        {
-            $this->novoView($usluga,'Cijena mora biti pozitivan broj ');
-            return;
-        }
-
-        Usluga::dodajNovi($usluga);
-        $this->index();
+           {
+               $this->noviEntitet();
+               return;
+           }
+                
+            
     }
 
-    private function novoView($usluga, $poruka)
+    private function noviEntitet()
+    {
+        $this->entitet = new stdClass();
+        $this->entitet->vrsta='';
+        $this->entitet->proizvod='';
+        $this->entitet->opis='';
+        $this->entitet->cijena='';
+        $this->entitet='Unesite tražene podatke';
+        $this-> novoView();
+        return;
+    }
+    
+
+    private function novaUsluga()
+    {
+        $this->usluga = new stdClass();
+        $this->usluga->vrsta='';
+        $this->usluga->proizvod='';
+        $this->usluga->opis='';
+        $this->usluga->cijena='';
+        $this->poruka='Unesite tražene podatke';
+        $this-> novoView();
+        return;
+    }
+
+    private function novoView()
     {
         $this->view->render($this->viewDir . 'novo',
         [
-            'usluga'=>$usluga,
-            'poruka'=>$poruka
+            'usluga'=>$this->usluga,
+            'poruka'=>$this->poruka
         ]);
-        return;
+    
     }
+
 }
--- app/model/Usluga.php
+++ app/model/Usluga.php
@@ -7,20 +7,24 @@ class Usluga
         $veza= DB::getInstanca();
         $izraz=$veza->prepare('
         
-        select * from usluga
+        select a.sifra, b.vrsta, a.proizvod, a.opis, a.cijena
+        from usluga a
+        inner join specijalizacija b on a.sifra =b.sifra
         
         ');
         $izraz->execute();
         return $izraz->fetchAll();
     }
 
-    public static function dodajNovi($usluga)
+
+
+    public static function dodajNovo($usluga)
     {
         $veza= DB::getInstanca();
         $izraz=$veza->prepare('
         
-        insert into usluga (naziv,vrsta,cijena)
-        values (:naziv,:vrsta,:cijena)
+        insert into usluga (vrsta,proizvod,opis,cijena)
+        values (:vrsta,:proizvod,:opis,:cijena)
         
         ');
         $izraz->execute((array)$usluga);

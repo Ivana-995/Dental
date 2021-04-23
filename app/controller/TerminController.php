c895d9503297d25963bfccdd77df21608f43d794
Ivana-995 <ilalic110@gmail.com>
2021-04-23 15:23:02 +0200

    Termin - izmjena (samo za čitanje stranica)

--- app/controller/TerminController.php
+++ app/controller/TerminController.php
@@ -7,31 +7,6 @@ class TerminController extends AutorizacijaController
                       . 'termin'
                       . DIRECTORY_SEPARATOR;
     
-    private $entitet=null;
-    private $poruka='';
-    private $pacijenti=null;
-    private $stomatolozi=null;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->pacijenti=Pacijent::ucitajSve();
-        
-        $s=new stdClass();
-        $s->sifra=-1;
-        $s->ime=' - - - -';
-        $s->prezime='';
-        array_unshift($this->pacijenti,$s);
-
-        $this->stomatolozi=Stomatolog::ucitajSve();
-        
-        $s=new stdClass();
-        $s->sifra=-1;
-        $s->ime=' - - - -';
-        $s->prezime='';
-        array_unshift($this->stomatolozi,$s);
-    }
-
     public function index()
     {
         $this->view->render($this->viewDir . 'index',
@@ -40,64 +15,9 @@ class TerminController extends AutorizacijaController
         ]);
     }
 
-    public function novo()
-    {
-        if($_SERVER['REQUEST_METHOD']==='GET')
-        {
-            $this->noviEntitet();
-            return;
-        }
-        $this->entitet = (object) $_POST;
-        try
-        {
-            $this->kontrola();
-            Termin::dodajNovo($this->entitet);
-            $this->index();
-        }
-        catch (Exception $e)
-        {
-            $this->poruka=$e->getMessage();
-            $this->novoView();
-        }
+}  
 
-    }
+   
+    
 
-    private function noviEntitet()
-    {
-        $this->entitet = new stdClass();
-        $this->entitet->pacijent=-1;
-        $this->entitet->stomatolog=-1;
-        $this->poruka='Odaberite pacijenta i stomatologa';
-        $this->novoView();
-    }
-
-    private function novoView()
-    {
-        $this->view->render($this->viewDir . 'novo',[
-            'entitet'=>$this->entitet,
-            'poruka'=>$this->poruka,
-            'pacijenti'=>$this->pacijenti,
-            'stomatolozi'=>$this->stomatolozi
-        ]);
-    }
-
-    private function kontrola()
-    {
-        $this->kontrolaPacijent();
-        $this->kontrolaStomatolog();
-    }
-
-    private function kontrolaPacijent()
-    {
-        if($this->entitet->pacijent==-1){
-            throw new Exception('Pacijent obavezno');
-        }
-    }
-
-    private function kontrolaStomatolog()
-    {
-        if($this->entitet->stomatolog==-1){
-            throw new Exception('Stomatolog obavezno');
-        }
-    }
-}
+  
--- app/model/Termin.php
+++ app/model/Termin.php
@@ -9,28 +9,15 @@ class Termin
         
 
         select concat(b.ime, \' \', b.prezime) as stomatolog,
-        concat(d.ime, \' \', d.prezime) as pacijent
+        concat(d.ime, \' \', d.prezime) as pacijent,
+        b.specijalizacija
         from termin a 
         inner join stomatolog b on a.stomatolog=b.sifra
         right join ordinacija c on b.ordinacija=c.sifra
         inner join pacijent d on a.pacijent=d.sifra
-      
 
         ');
         $izraz->execute();
         return $izraz->fetchAll();
-    }
-
-    public static function dodajNovo($entitet)
-    {
-        $veza = DB::getInstanca();
-        $izraz=$veza->prepare('
-        
-            insert into termin (pacijent,stomatolog)
-            values (:pacijent, :stomatolog)
-        
-        ');
-        $izraz->execute((array)$entitet);
-        return $veza->lastInsertId();
     }
 }
